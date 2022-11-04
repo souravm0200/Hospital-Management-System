1ad68a8f82408fa900f6e8280c3f1ae88cbf68e5
SOURAV MANDAL <sourav0200@gmail.com>
2022-11-04 21:25:55 +0600

    C# project

---
+++ hospitalmanagementsystemcsharp/HospitalManagementSystemCSharp/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace HospitalManagementSystemCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {           
+                if (textBox1.Text == "nss" || textBox2.Text == "12345")
+                {
+                    MessageBox.Show(" You are logged in successfully.");
+                    this.Visible = false;
+                    Home obj1 = new Home();
+                    obj1.ShowDialog();
+                    textBox1.Text = "";
+                    textBox2.Text = "";                 
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Username Or Password.");
+                }
+         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
